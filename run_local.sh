c75fab3df9fea1b8d55e35fb0424db9ab2df890e
mlpierce22 <mlpierce22@gmail.com>
2024-01-27 13:15:50 -0800

    update run_local for dockerfile

--- run_local.sh
+++ run_local.sh
@@ -12,26 +12,24 @@ fi
 
 export USE_MODEL
 
-python main.py $USE_MODEL
+# Define your tasks here. Each task should be a function
+run_watcher() {
+    echo "Running watcher with model $USE_MODEL"
+    python main.py "$USE_MODEL"
+}
 
-# # Define your tasks here. Each task should be a function
-# run_watcher() {
-#     echo "Running watcher with model $USE_MODEL"
-#     python main.py $USE_MODEL
-# }
+run_ollama() {
+    echo "Running ollama with model $USE_MODEL"
+    ollama run "$USE_MODEL"
+}
 
-# run_ollama() {
-#     # echo "Running ollama with model $USE_MODEL"
-#     # ollama run $USE_MODEL
-# }
+# Store the names of the task functions in an array
+tasks=(run_watcher run_ollama) # Add more task names to this array as needed
 
-# # Store the names of the task functions in an array
-# tasks=(run_watcher run_ollama) # Add more task names to this array as needed
-
-# # Export the functions
-# for task in "${tasks[@]}"; do
-#     export -f "$task"
-# done
+# Export the functions
+for task in "${tasks[@]}"; do
+    export -f "$task"
+done
 
 # Run the tasks in parallel with --line-buffer for immediate output and --halt now,fail=1 to stop immediately on failure
-# parallel --line-buffer --results logfiles --halt now,fail=1 ::: "${tasks[@]}"
+parallel --line-buffer --halt now,fail=1 ::: run_watcher run_ollama
