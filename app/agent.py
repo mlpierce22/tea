6de8ec12e0d88ae92292589aa7b196c4242b26a8
mlpierce22 <mlpierce22@gmail.com>
2024-02-01 19:21:24 -0800

    Add thinking code tests

--- app/agent.py
+++ app/agent.py
@@ -125,6 +125,7 @@ class TeaAgent():
         steep_component = create_steep_component()
         tea_component = create_tea_component()
         available_components = get_available_components(ctx.root_directory)
+        # thought_process = self.requirements_thinking(ctx, available_components=available_components)
 
         prompt = write_component_prompt(
             user_query=ctx.tea_tag.children,
@@ -161,6 +162,10 @@ class TeaAgent():
 
         full_response = self._process_response(self.llm, prompt)
 
+        # Make model come up with requirements
+        # thought_process = self.requirements_thinking(ctx, available_components=available_components)
+
+        # code = self.generate_code(ctx, thought_process)
         # Grab the code from between the backticks
         code = full_response.split("```")[1].strip("\n")
 
@@ -168,12 +173,12 @@ class TeaAgent():
         if code[:3] == "vue":
             code = code[4:] # Also remove newline
 
-        if self.should_think:
-            check = self.double_check(ctx, generated_component_code=code, available_components=available_components)
-            if check == True:
-                log.info("The component was approved!")
-            else:
-                code = check
+        # if self.should_think:
+        #     check = self.double_check(ctx, generated_component_code=code, available_components=available_components)
+        #     if check == True:
+        #         log.info("The component was approved!")
+        #     else:
+        #         code = check
 
         # Once we get the response, we want to write it to the file
         with open(ctx.steep_path, "w") as file:
@@ -202,3 +207,103 @@ class TeaAgent():
             return True
         else:
             return code
+
+    def requirements_thinking(self, ctx: SteepContext, available_components: str = None):
+        """
+            Takes a user's query and expands it to think about the requirements and constraints
+        """
+
+#         prompt = f"""
+# You are a pragmatic principal open-source frontend engineer specializing in the Vue ecosystem. Your task is to take the user's query and expand it to think about the requirements, constraints, and make assumptions.
+
+# Below is the parent component. it uses the <Tea> component to render the component that you make a plan to create. Copy the style and syntax decisions but DO NOT COPY THIS COMPONENT VERBATIM.
+# ```
+# {ctx.file_content}
+# ```
+
+# Some of this component has potentially already been written. Here is the starter code:
+# ```
+# {ctx.steep_content}
+# ```
+
+# You have access to the following packages:
+# ```
+# {ctx.packages}
+# ```
+
+# {available_components if available_components else ''}
+
+# The user's query is:
+# ```
+# {ctx.tea_tag.children}
+# ```
+
+# An engineer will implement the outline you generate in a Vue file. The component should be as simple and easy to understand as possible, while still fully expressing the functionality required. It is not your job to write code! You should choose reasonable defaults and make reasonable assumptions where necessary. You should try to use the packages and components provided to you, wherever possible.
+
+# For example, if the user query is `Generate a 3 by 3 grid of images`, your output might be:
+# ```markdown
+# # Overall Instructions and assumptions:
+# - The user wants a 3 by 3 grid of images.
+# - The user likely wants the images to be responsive, square, evenly spaced, and fill the screen.
+# - The NuxtImg and UContainer components are available as a component and therefore should be used. No import statement is required because this is a Nuxt component.
+# - Based on the parent component, it looks like Tailwind is used in this repository, that should be relied on for styling.
+
+# # Script section:
+# - The parent uses the script setup syntax, so the component should use the script setup syntax.
+# - The image urls is not passed in as a prop, and therefore should be represented as an array of strings inside this component. There should be 9 image urls in the array.
+
+# # Template
+# - Use TailwindCSS to create a 3 column grid.
+# - Loop through the array of image urls in a v-for loop.
+
+# # Style:
+# - Relying on tailwind classes for now since they are present in this repository
+# ```
+
+# Now it's your turn. The plan for this component is as follows:
+#         """
+        prompt = f"""
+You are a pragmatic principal open-source frontend engineer specializing in the Vue ecosystem. Your task is to take the user's query and expand it to think about the requirements, constraints, and make assumptions.
+
+User's Query:
+```
+{ctx.tea_tag.children}
+```
+"""
+        log.info("The requirements thinking prompt!")
+        log.info(prompt)
+        log.info("\n=====\n")
+        return self._process_response(self.llm, prompt)
+
+    def generate_code(self, ctx: SteepContext, thought_process: str):
+        """
+            Generates the code for the component
+        """
+        prompt = f"""
+You are a pragmatic principal open-source frontend engineer specializing in the Vue ecosystem.
+You are about to get detailed instructions for code to write to generate {ctx.steep_path}.
+
+# Instructions:
+===
+{thought_process}
+===
+
+# Rules:
+===
+- There should be NO MORE THAN 1 <script> tag, NO MORE THAN 1 <template> tag, and NO MORE THAN 1 <style> tag in the output file.
+- DO NOT add any new libraries or assume any classes or components that you don't see, other than those clearly used by the component. Put everything into this single file: styles, types, etc.
+- The code should be complete and fully functional. NO PLACEHOLDERS. DO NOT ADD ANY COMMENTS. NO OMISSIONS. Any omission or deviation will completely break the system.
+- DO NOT OMIT ANYTHING FOR BREVITY
+- DO NOT OMIT ANYTHING FOR BREVITY
+- DO NOT OMIT ANYTHING FOR BREVITY
+- Output a whole new Vue file within triple backticks (```) AND NOTHING ELSE.
+- The ouputted code should be complete and fully functional. The code you output will be written directly to a file, as-is.
+- The code must make sense and be easy to understand. It should be as simple as possible, but no simpler.
+===
+
+Output whole new component file WITHIN triple backticks (```) AND NOTHING ELSE. It will be saved as is to the component file and should work out of the box.
+        """
+        log.info("The generate code prompt!")
+        log.info(prompt)
+        log.info("\n=====\n")
+        return self._process_response(self.llm, prompt)
