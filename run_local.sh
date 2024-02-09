d4904dbf76d11f28242e3054d5a5acdf173af4c2
mlpierce22 <mlpierce22@gmail.com>
2024-02-09 12:44:39 -0800

    NOOP shell formatting

--- run_local.sh
+++ run_local.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-if [ -z "$MODEL" ]; then
-    MODEL="deepseek-coder:6.7b-instruct"
-    echo "Using \"$MODEL\" by default, you can specify a model with:"
-    echo "bash $0 <ollama_model>"
-    echo "See available models at https://ollama.ai/library"
-    export MODEL
+if [[ -z ${MODEL} ]]; then
+	MODEL="deepseek-coder:6.7b-instruct"
+	echo "Using \"${MODEL}\" by default, you can specify a model with:"
+	echo "bash $0 <ollama_model>"
+	echo "See available models at https://ollama.ai/library"
+	export MODEL
 fi
 
 # Define your tasks here. Each task should be a function
 run_watcher() {
-    echo "Running watcher"
-    python app/main.py
+	echo "Running watcher"
+	python app/main.py
 }
 
 run_ollama() {
-    # Check if the OPENAI_KEY environment variable is not set before running ollama
-    if [ -z "$OPENAI_KEY" ]; then
-        echo "Running ollama with model $MODEL"
-        ollama run "$MODEL"
-    else
-        echo "Skipping ollama run due to OPENAI_KEY being set"
-    fi
+	# Check if the OPENAI_KEY environment variable is not set before running ollama
+	if [[ -z ${OPENAI_KEY} ]]; then
+		echo "Running ollama with model ${MODEL}"
+		ollama run "${MODEL}"
+	else
+		echo "Skipping ollama run due to OPENAI_KEY being set"
+	fi
 }
 
 # Pull the model first
-ollama pull "$MODEL"
+ollama pull "${MODEL}"
 
 # Store the names of the task functions in an array
 tasks=(run_watcher run_ollama) # Add more task names to this array as needed
 
 # Export the functions
 for task in "${tasks[@]}"; do
-    export -f "$task"
+	export -f "${task?}"
 done
 
 # Run the tasks in parallel with --line-buffer for immediate output and --halt now,fail=1 to stop immediately on failure
